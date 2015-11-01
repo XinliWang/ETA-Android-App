2de577a147287472ebc223ca762e24d1f0a5fb65
Xinli Wang <wxlmaple@hotmail.com>
2015-11-01 01:03:35 -0500

    add friends from contacts

---
+++ ETA-CS9033/app/src/main/java/com/nyu/cs9033/eta/db/TripDatabaseHelper.java
@@ -0,0 +1,118 @@
+package com.nyu.cs9033.eta.db;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.location.Location;
+
+import com.nyu.cs9033.eta.models.Trip;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+
+public class TripDatabaseHelper extends SQLiteOpenHelper {
+    private static final int DATABASE_VERSION = 1;
+    private static final String DATABASE_NAME = "trips";
+
+    private static final String TABLE_TRIP = "trip";
+    private static final String COLUMN_TRIP_ID = "_id"; // convention
+    private static final String COLUMN_TRIP_NAME = "name";
+    private static final String COLUMN_TRIP_TIME = "time";
+    private static final String COLUMN_TRIP_FRIENDS = "friends";
+    private static final String COLUMN_TRIP_DESTINATION = "destination";
+
+    private static final String TABLE_LOCATION = "location";
+    private static final String COLUMN_LOC_TRIPID = "trip_id";
+    private static final String COLUMN_LOC_TIMESTAMP = "timestamp";
+    private static final String COLUMN_LOC_LAT = "latitude";
+    private static final String COLUMN_LOC_LONG = "longitude";
+    private static final String COLUMN_LOC_ALT = "altitude";
+    private static final String COLUMN_LOC_PROVIDER = "provider";
+
+
+    public TripDatabaseHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        //create trip table
+        String tripTable = "create table " + TABLE_TRIP + "("
+            + COLUMN_TRIP_ID + " integer primary key autoincrement, "
+            + COLUMN_TRIP_NAME + " text, "
+            + COLUMN_TRIP_TIME + " text, "
+            + COLUMN_TRIP_DESTINATION + " text, "
+            + COLUMN_TRIP_FRIENDS + " text)";
+
+        db.execSQL(tripTable);
+
+        // create location table
+        String locationTable = "create table " + TABLE_LOCATION + "("
+            + COLUMN_LOC_TRIPID + " integer references trip(_id), "
+            + COLUMN_LOC_TIMESTAMP + " integer, "
+            + COLUMN_LOC_LAT + " real, "
+            + COLUMN_LOC_LONG + " real, "
+            + COLUMN_LOC_ALT + " real, "
+            + COLUMN_LOC_PROVIDER + " varchar(100))";
+
+        db.execSQL(locationTable);
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        // Drop older table if exists
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_TRIP);
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_LOCATION);
+        // create tables again
+        onCreate(db);
+
+    }
+
+    public long insertTrip(Trip trip) {
+        ContentValues cv = new ContentValues();
+        cv.put(COLUMN_TRIP_NAME,trip.getName());
+        cv.put(COLUMN_TRIP_TIME, trip.getTime());
+        cv.put(COLUMN_TRIP_DESTINATION, trip.getDestination());
+        cv.put(COLUMN_TRIP_FRIENDS, trip.getFriends());
+        // return id of new trip
+        return getWritableDatabase().insert(TABLE_TRIP, null, cv);
+    }
+
+    public long insertLocation(long tripId, Location location) {
+        ContentValues cv = new ContentValues();
+        cv.put(COLUMN_LOC_TRIPID, tripId);
+        cv.put(COLUMN_LOC_TIMESTAMP, location.getTime());
+        cv.put(COLUMN_LOC_LAT, location.getLatitude());
+        cv.put(COLUMN_LOC_LONG, location.getLongitude());
+        cv.put(COLUMN_LOC_ALT, location.getAltitude());
+        cv.put(COLUMN_LOC_PROVIDER, location.getProvider());
+        // return id of new location
+        return getWritableDatabase().insert(TABLE_LOCATION, null, cv);
+    }
+
+    public List<Trip> getAllTrips() {
+        List<Trip> tripList = new ArrayList<Trip>();
+
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor cursor = db.rawQuery("select * from " + TABLE_TRIP, null);
+
+        // loop through all query results
+        for (cursor.moveToFirst(); !cursor.isAfterLast(); cursor.moveToNext()) {
+            Trip trip = new Trip();
+            trip.setId(cursor.getInt(0));
+            trip.setName(cursor.getString(1));
+            trip.setTime(cursor.getString(2));
+            trip.setDestination(cursor.getString(3));
+            trip.setFriends(cursor.getString(4));
+            tripList.add(trip);
+        }
+        return tripList;
+    }
+
+}
---
+++ ETA-CS9033/app/src/main/java/com/nyu/cs9033/eta/models/Person.java
@@ -0,0 +1,125 @@
+package com.nyu.cs9033.eta.models;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class Person implements Parcelable {
+	
+	// Member fields should exist here, what else do you need for a person?
+	// Please add additional fields
+    //private int id;
+	private String name;
+    //private String location;
+    //private long phone;
+
+//    public int getId() {
+//        return id;
+//    }
+//
+//    public void setId(int id) {
+//        this.id = id;
+//    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+//    public String getLocation() {
+//        return location;
+//    }
+//
+//    public void setLocation(String location) {
+//        this.location = location;
+//    }
+//
+//    public long getPhone() {
+//        return phone;
+//    }
+//
+//    public void setPhone(long phone) {
+//        this.phone = phone;
+//    }
+
+    /**
+	 * Parcelable creator. Do not modify this function.
+	 */
+	public static final Parcelable.Creator<Person> CREATOR = new Parcelable.Creator<Person>() {
+		public Person createFromParcel(Parcel p) {
+			return new Person(p);
+		}
+
+		public Person[] newArray(int size) {
+			return new Person[size];
+		}
+	};
+	
+	/**
+	 * Create a Person model object from a Parcel. This
+	 * function is called via the Parcelable creator.
+	 * 
+	 * @param p The Parcel used to populate the
+	 * Model fields.
+	 */
+	public Person(Parcel p) {
+		
+		// TODO - fill in here
+        name = p.readString();
+//		location = p.readString();
+//        phone = p.readLong();
+	}
+	
+	/**
+	 * Create a Person model object from arguments
+	 * 
+	 * @param name Add arbitrary number of arguments to
+	 * instantiate Person class based on member variables.
+	 */
+	public Person(String name) {
+		
+		// TODO - fill in here, please note you must have more arguments here
+        this.name = name;
+//        this.location = location;
+//        this.phone = phone;
+	}
+
+	/**
+	 * Serialize Person object by using writeToParcel.  
+	 * This function is automatically called by the
+	 * system when the object is serialized.
+	 * 
+	 * @param dest Parcel object that gets written on 
+	 * serialization. Use functions to write out the
+	 * object stored via your member variables. 
+	 * 
+	 * @param flags Additional flags about how the object 
+	 * should be written. May be 0 or PARCELABLE_WRITE_RETURN_VALUE.
+	 * In our case, you should be just passing 0.
+	 */
+	@Override
+	public void writeToParcel(Parcel dest, int flags) {
+		
+		// TODO - fill in here
+        dest.writeString(this.name);
+        //dest.writeString(this.location);
+        //dest.writeLong(this.phone);
+	}
+	
+	/**
+	 * Feel free to add additional functions as necessary below.
+	 */
+	public Person(){
+
+    }
+	/**
+	 * Do not implement
+	 */
+	@Override
+	public int describeContents() {
+		// Do not implement!
+		return 0;
+	}
+}
